4afb6fc11099faff63f560eb0a6ac7f501719ecc
KaramveerSinghRathore <karmveersingh.aug23@gmail.com>
2026-03-31 18:17:35 +0530

    Moving Zeroes

---
+++ MovingZero.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        while(j < nums.size()) {
+            nums[j] = 0;
+            j++;
+        }
+    }
+};
+int main() {
+    vector<int> nums = {0, 1, 0, 3, 12};
+    Solution sol;
+    sol.moveZeroes(nums);
+    cout << "Array after moving zeroes: ";
+    for(int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
